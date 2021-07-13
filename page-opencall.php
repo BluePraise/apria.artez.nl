eb76dab37f87bda48afe45e1ade89eac0db5a3c0
Gangesh Matta <gangeshmatta@gmail.com>
2021-07-13 21:04:34 +0530

    updated open-call page

--- page-opencall.php
+++ page-opencall.php
@@ -26,15 +26,44 @@ Template Name: Open-Call
 // All access is handled by main.php (usually triggered from .htaccess)
 
 
-define('WORDPRESS', true);
+wp_head();
+
+$content = get_post(get_the_ID());
+if($content){
+	$page = (object)[
+		'title' => get_the_title(),
+		'subtitle' => get_field('subtitle', $id),
+		'content' => apply_filters('the_content', $content->post_content),
+		'paragraph_layout' => get_field('paragraph_layout'),
+	];
+}
+?>
 
 
-// Set request variables accordingly
-$_GET = array(
-	'realm' => 'opencall',
-	'action' => 'show',
-	'id' => get_the_ID(),
-);
+<div class="main-content subscribe-content">
+	<div class="overlay-close-button">
+		<a href="<?=get_home_url(); ?>" class="js-opencall-close-button">
+			<img src="/wp-content/themes/apria/elements/icon_close_black.svg">
+		</a>
+	</div>
 
-// Redirect request to main.php controller
-require __DIR__ . '/main.php';
+	<a href="<?=get_home_url() ?>" class="logo" style="background-color: #">
+		<img src="/wp-content/themes/apria/elements/logo.svg" alt="">
+	</a>
+
+	<article class="main-column">
+		<div class="content-wrap">
+			<div class="article__head">
+				<h1 class="article__title"> <?=$page->title; ?></h1>
+<?php if($page->subtitle): ?>
+				<h2 class="article__subtitle"><?=$page->subtitle; ?></h2>
+<?php endif; ?>
+			</div>
+
+			<div class="article__text<?php if($page->paragraph_layout == 'indents'){ ?>article__text-indents <?php } ?> article__text--open-call">
+				<?=$page->content; ?>
+			</div>
+		</div>
+	</article>
+</div>
+
