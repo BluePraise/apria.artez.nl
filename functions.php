d121bebb7a1596f7d8ec16b245c0f6b892c1debc
Magalie Chetrit <1594036+BluePraise@users.noreply.github.com>
2021-07-21 22:06:48 +0200

    navigation and hero section

--- functions.php
+++ functions.php
@@ -90,9 +90,9 @@ add_filter('acf/settings/save_json', 'my_acf_json_save_point');
 if (function_exists('acf_add_options_page')) {
 
 	acf_add_options_page(array(
-		'page_title' => 'Theme General Settings',
-		'menu_title' => 'Theme Settings',
-		'menu_slug' => 'theme-general-settings',
+		'page_title' => 'Apria Information',
+		'menu_title' => 'Apria Information',
+		'menu_slug'  => 'apria-information',
 		'capability' => 'edit_posts',
 		'capability' => 'edit_posts',
 		'position' => '8',
--- home.php
+++ home.php
@@ -6,33 +6,45 @@ get_header();
 // <p><?=get_field("intro_text");
 ?>
 <main>
+	<section class="hero">
+	<div class="logo-container">
+		<a href="<?php echo esc_url(home_url()); ?>" class="logo">
+			<svg class="apria_logo"></svg>
+		</a>
+	</div>
+	<p class="intro-text">APRIA: ArtEZ Platform for Research Interventions of the Arts is an online platform that curates a peer-reviewed journal (APRIA journal) and publishes high-impact essays, image and sound contributions that examine art and interventions of the arts in relation to science and society, and that encourage dialogue around themes that are critical and urgent to the futures that we will live in.</p>
+	<div class="news-column">
+		<h2>NEWS</h2>
+		<?php if(get_field("news_text")): ?>
+			<p class="news-text"><?php echo get_field("news_text"); ?></p>
+		<?php endif; ?>
+	</div>
+	</section>
+	<section class="posts">
+		<ul>
+			<?php
+			global $post;
 
-<div class="logo-container">
-	<a href="<?php echo esc_url(home_url()); ?>" class="logo" style="background-color: #000000">
-		<svg class="apria_logo"></svg>
-	</a>
-</div>
-<ul>
-    <?php
-    global $post;
+			$myposts = get_posts( array(
+				'posts_per_page' => 5,
+				'offset'         => 1,
+				'category'       => 1
+			) );
 
-    $myposts = get_posts( array(
-        'posts_per_page' => 5,
-        'offset'         => 1,
-        'category'       => 1
-    ) );
+			if ( $myposts ) {
+				foreach ( $myposts as $post ) :
+					setup_postdata( $post ); ?>
+					<li><a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></a></h3></li>
+				<?php
+				endforeach;
+				wp_reset_postdata();
+			}
+			?>
 
-    if ( $myposts ) {
-        foreach ( $myposts as $post ) :
-            setup_postdata( $post ); ?>
-            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-        <?php
-        endforeach;
-        wp_reset_postdata();
-    }
-    ?>
+		</ul>
 
-</ul>
+	</section>
+
 </main>
 
 
