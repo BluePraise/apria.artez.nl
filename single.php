a88c0978cce706987ddc7986221137eb02b5d3dd
Magalie Chetrit <first@mcmcmcmcmc.anonaddy.com>
2021-08-15 12:37:29 +0200

    styling and refactorng for single css

--- single.php
+++ single.php
@@ -37,61 +37,47 @@ get_header();
 
 <main>
 	<article>
-		<div class="content-wrap">
-			<div class="article__head">
-				<div class="head__top-line" >
-					<span class="article__date"><?=get_the_date('d-M-Y'); ?></span>
-					<span class="article__authors">
-			<?php		foreach ($authors as $aAuthor) { ?>
-			<a href="<?=get_site_url().'/author/'.$aAuthor->user_nicename.'/'; ?>">
-				<?=$aAuthor->display_name; ?> </a>
-	
-	<?php } ?>
-						</span>
-
-<?php 
-if(get_field('issue')): 
-	//var_dump(get_field('issue'));
-	$article_issue = get_field('issue');
-	
-	?>
+		<header class="article-header">
+			<span class="article__date"><?=get_the_date('d-M-Y'); ?></span>
+			<span class="article__authors">
+				<?php foreach ($authors as $aAuthor): ?>
+					<a href="<?=get_site_url().'/author/'.$aAuthor->user_nicename.'/'; ?>"><?=$aAuthor->display_name; ?></a>
+				<?php endforeach; ?>
+			</span>
+			<?php 
+				if(get_field('issue')): 
+					$article_issue = get_field('issue');?>
 					<span class="article__source">
 						Published in<br />
 						<?=$article_issue->post_title; ?>
 					</span>
-<?php endif; ?>
-
-<?php if(get_field('doi')): ?>
+				<?php endif; ?>
+			
+				<?php if(get_field('doi')): ?>
 					<span class="article__issue">
 						<a href="https://doi.org/{$article->doi|trim}">
-							 <?=get_field('doi'); ?>
+							<?=get_field('doi'); ?>
 						</a>
 					</span>
-<?php endif; ?>
-				</div>
-				<h1 class="content-title balance-text"><?php the_title(); ?></h1>
-<?php if(get_field('subtitle')): ?>
-				<h2 class="article__subtitle balance-text"><?=get_field('subtitle'); ?></h2>
-<?php endif; ?>
-				<div class="head__bottom-line">
-
-
-
+				<?php endif; ?>
+		</header>
+		<h1 class="content-title"><?php the_title(); ?></h1>
+		<?php if(get_field('subtitle')): ?>
+			<h2 class="subtitle"><?=get_field('subtitle'); ?></h2>
+		<?php endif; ?>
+		<div class="tag-list">
+				<?php if($article_tags): $i = 0; ?>
+					<?php foreach($article_tags as $aTag): ?>
+						<a class="article__tag" href="<?=$aTag->url; ?>"><?=$aTag->name; ?></a> 
+							<?php if(++$i !== count($article_tags)):
+								echo '<span class="article-tag-separator">/</span>';
+							endif; ?>
+					<?php endforeach; ?>
 				
-<?php if($article_tags): $i = 0; ?>
-					<span class="article__tags">
-<?php foreach($article_tags as $aTag) { ?>
-						<a href="<?=$aTag->url; ?>"><?=$aTag->name; ?></a> 
-						<?php if(++$i !== count($article_tags)) {
-    echo "/";
-  } ?>
-<?php } ?>
-					</span>
-<?php endif; ?>
-				
-				</div>
+				<?php endif; ?>
 			</div>
-			<div class="article__text{if $article->paragraph_layout == 'indents'} article__text-indents{/if}">
+			
+			<div class="article__text">
 				<?=$content; ?>
 			</div>
 <?php /* get_footer('article');*/ ?>
@@ -114,18 +100,17 @@ if($authors):
 			</div>
 <?php endif; 
 
-
-if ($footnotes):  ?>
+		if ($footnotes):  ?>
 			<div class="article__footnotes">
 				<div class="footnotes__headline">References</div>
-<?php foreach($footnotes as $aFootnote) { ?>
-				<div class="footnote">
-					<span class="footnote-up icon js-footnote-up" data-footnotetext="{$aFootnote@iteration}">↑</span>
-					<p><?=$aFootnote; ?></p>
-				</div>
-<?php } ?>
-			</div>
-<?php endif; ?>
+				<?php foreach($footnotes as $aFootnote) : ?>
+					<div class="footnote">
+						<span class="footnote-up icon js-footnote-up" data-footnotetext="{$aFootnote@iteration}">↑</span>
+						<p><?=$aFootnote; ?></p>
+					</div>
+				<?php endforeach; ?>
+			</div><!-- article__footnotes -->
+	<?php endif; ?>
 
 <?php if(get_field('bibliography')): ?>
 			<div class="article__bibliography">
