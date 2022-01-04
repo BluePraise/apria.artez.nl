8e144f93960abd241fd2e6d7c9912dc8b57ddde4
Magalie Chetrit <first@mcmcmcmcmc.anonaddy.com>
2022-01-04 10:41:45 -0500

    WIP - Added Newsletter HTML from Mailchimp

--- page-newsletter.php
+++ page-newsletter.php
@@ -17,46 +17,50 @@ get_header();
 <main class="page-view">
 	<div class="overlay-close-button js-newsletter-close-button">
 		<a href="<?=get_home_url() ?>" >
-			<img src="/wp-content/themes/apria/elements/icon_close_black.svg">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/icon_close_black.svg">
 		</a>
 	</div>
 
 	<article>
-	
+
 			<h1 class="content-title">Subscribe to our newsletter to stay up-to-date with the APRIA Journal and Platform.</h1>
 
-			<div class="tnp">
-				<form method="post" id="newsletter" action="http://apriawp1.local/?na=s" onsubmit="return newsletter_check(this)" class="subscribe-form">
 
-					<input type="hidden" name="nlang" value="">
-					<div class="tnp-field tnp-field-firstname input-wrap input--half">
-
-						<input class="tnp-firstname form__input" type="text" name="nn" >
-						<label class="form__label">First name</label>
-					</div>
-
-					<div class="tnp-field tnp-field-lastname input-wrap input--half">
-
-						<input class="tnp-lastname form__input" type="text" name="ns" >
-						<label class="form__label">Last name</label>
-					</div>
-
-					<div class="tnp-field tnp-field-email input-wrap">
-
-						<input class="tnp-email form__input" type="email" name="ne" required>
-						<label class="form__label">Email</label>
-					</div>
-
-
-					<div class="tnp-field tnp-field-button">
-						<input class="tnp-submit form__submit" type="submit" value="Subscribe">
-					</div>
-				</form>
+				<!-- Begin APRIA Mailchimp Signup Form -->
+<div id="mc_embed_signup">
+    <form action="https://artez.us20.list-manage.com/subscribe/post?u=900faf96fb1c9ed5b73a7586b&amp;id=4baf0f7cd7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" novalidate>
+        <div id="mc_embed_signup_scroll">
+		<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+		<div class="mc-field-group input-wrap input--half">
+			<input type="text" value="" name="FNAME" class="form__input" id="mce-FNAME">
+			<label for="mce-FNAME" class="form__label">First Name </label>
+		</div>
+		<div class="mc-field-group input-wrap input--half">
+			<input type="text" value="" name="LNAME" class="form__input" id="mce-LNAME">
+			<label for="mce-LNAME" class="form__label">Last Name </label>
+		</div>
+		<div class="mc-field-group input-wrap">
+			<input type="email" value="" name="EMAIL" class="form__input required email" id="mce-EMAIL">
+			<label for="mce-EMAIL" class="form__label">Email Address  <span class="asterisk">*</span></label>
+		</div>
+		<div id="mce-responses" class="clear foot">
+			<div class="response" id="mce-error-response" style="display:none"></div>
+			<div class="response" id="mce-success-response" style="display:none"></div>
+		</div>                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+		<div style="position: absolute; left: -5000px;" aria-hidden="true">
+			<input type="text" name="b_900faf96fb1c9ed5b73a7586b_4baf0f7cd7" tabindex="-1" value=""></div>
+			<div class="optionalParent">
+				<div class="clear foot tnp-field-button">
+					<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button form__submit">
+				</div>
 			</div>
+		</div>
+	</form>
+</div><!--End mc_embed_signup-->
 
-		
+
 	</article>
-	
+
 </main>
 
 <?php get_footer(); ?>
